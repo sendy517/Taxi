b59762df42a28cf27b626c8c9373a55816ec0dfc
seendy <seendy.prog@gmail.com>
2024-04-08 21:57:47 +1000

    Taxi v0.1.4

--- server/config.py
+++
@@ -1,5 +0,0 @@
-host="77.221.143.210",
-user="senid517",
-password="xyi1234",
-db_name="taxi",
-port="5433"
--- server/main.py
+++ server/main.py
@@ -1,24 +1,36 @@
 import psycopg2
-from config import host, user, password, db_name
+from dotenv import load_dotenv
+import os
 
+# Загрузка переменных окружения из файла .env
+dotenv_path = os.path.join(os.path.dirname(__file__), 'connection.env')
+load_dotenv(dotenv_path)
+
+# Подключение к базе данных
 try:
-    connection=psycopg2.connect(
-            host=host,
-            user=user,
-            password=password,
-            database=db_name
+    conn = psycopg2.connect(
+        host=os.getenv("DB_HOST"),
+        user=os.getenv("DB_USER"),
+        password=os.getenv("DB_PASSWORD"),
+        dbname=os.getenv("DB_NAME"),
+        port=os.getenv("DB_PORT")
     )
 
-    #cursor=connection.cursor()
-    with connection.cursor() as cursor:
-        cursor.execute(
-            "SELECT version();"
-        )
 
-        print(f"Server version: {cursor.fetchone()}")
-except Exception as _ex:
-    print("Ошибка подключения к бд", _ex)
+    # Создание курсора
+    cur = conn.cursor()
+
+    # Выполнение SQL-запроса для получения версии сервера
+    cur.execute("SELECT version();")
+
+    # Получение результата
+    version = cur.fetchone()
+    print("Подключение к серверу успешно. Версия сервера PostgreSQL:", version[0])
+
+    # Закрытие курсора и подключения
+    cur.close()
+except psycopg2.OperationalError as e:
+    print(f"Ошибка при подключении к базе данных: {e}")
 finally:
-    if connection:
-        connection.close()
-        print("[INFO] postgreSQL connection closed")
+    if conn:
+        conn.close()
